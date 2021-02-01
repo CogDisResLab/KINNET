85d53bbc957e42a5c62fa284df29ee27956b606b
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-01-31 20:03:06 -0500

    Added the PTK-Interactome dataset

---
+++ data-raw/ptk_interactome.R
@@ -0,0 +1,21 @@
+## code to prepare `ptk_interactome` dataset goes here
+
+library(tidyverse)
+library(devtools)
+
+interactome_file <- file.path("data-raw/data/RegPhos_kinase_PPI_human.txt")
+
+interactome <- read_tsv(interactome_file) %>%
+  select(GENE_a, GENE_b) %>%
+  rename(from = GENE_a,
+         to = GENE_b)
+
+interactome_na_to <- interactome$to %>%
+  tibble(from = NA, to = .)
+
+interactome_na_from <- interactome$from %>%
+  tibble(from = ., to = NA)
+
+ptk_interactome <- bind_rows(interactome, interactome_na_to, interactome_na_from)
+
+usethis::use_data(ptk_interactome, overwrite = TRUE)
