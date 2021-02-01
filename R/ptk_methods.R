648fc4c0ce724decf6f89ed4cdc68749477ffe4e
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-01-31 19:24:45 -0500

    Added methods for the PTK Chip data class

---
+++ R/ptk_methods.R
@@ -0,0 +1,81 @@
+#' @include pamchip_data_ptk.R
+#' @import methods stringr
+NULL
+
+setMethod(
+  "show",
+  signature = "PamchipData-PTK",
+  definition = function(object) {
+    print(
+      # stringr::str_glue(
+      #   "
+      #   {object@ChipType} PamChip data
+      #   {nrow(x@SampleData)} samples with {nrow(x@SampleCharacteristics$class)} distinct classes.
+      #   "
+      # )
+      stringr::str_glue("{object@ChipType} PamChip Data
+                        Processed with BioNavigator {object@BioNavigatorVersion}
+                        Processing done on {object@ImageAnalysisDate}
+                        {nrow(exp_data(object))} Observations in {length(unique(pheno_data(object)$class))} distinct classes
+                        ")
+    )
+    invisible(NULL)
+  }
+)
+
+setGeneric("processed_data", function(object, ...) {
+  standardGeneric("processed_data")
+})
+
+setMethod("processed_data", "PamchipData-PTK",
+          function(object) {
+            object@ProcessedData
+          })
+
+setGeneric("processed_data<-", function(object, value) {
+  standardGeneric("processed_data<-")
+})
+setMethod("processed_data<-", "PamchipData-PTK",
+          function(object, value) {
+            object@ProcessedData <- value
+            if (validObject(object)) {
+              return(object)
+            }
+          })
+
+setGeneric("pheno_data", function(object,...) {
+  standardGeneric("pheno_data")
+})
+
+setMethod("pheno_data", "PamchipData-PTK",
+          function(object) {
+            object@SampleCharacteristics
+          })
+
+setGeneric("exp_data", function(object,...) {
+  standardGeneric("exp_data")
+})
+
+setMethod("exp_data", "PamchipData-PTK",
+          function(object) {
+            object@SampleData
+          })
+
+setGeneric("classes", function(object,...) {
+  standardGeneric("classes")
+})
+
+setMethod("classes", "PamchipData-PTK",
+          function(object) {
+            unique(object@SampleCharacteristics$class)
+          })
+
+
+setGeneric("peptides", function(object,...) {
+  standardGeneric("peptides")
+})
+
+setMethod("peptides", "PamchipData-PTK",
+          function(object) {
+            object@PeptideIDs
+          })
