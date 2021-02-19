0b11fc53bd4dfc21a382d0ea17b72bcee81623aa
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-02-18 21:51:15 -0500

    Added new functions to calculate the distances

---
+++ R/distances.R
@@ -0,0 +1,37 @@
+#' Calculate the Structural Hamming Distance between two kinased graphs
+#'
+#' @param reference a reference result from assign_kinases()
+#' @param comparison a comparison result from assign_kinases
+#'
+#' @return A positive integer
+#' @export
+#'
+#' @examples
+#' TRUE
+kinnet_shd <- function(reference, comparison) {
+
+  pair <- equalize_kinase_graphs(reference, comparison)
+
+  res <- bnlearn::shd(pair$reference, pair$comparison)
+
+  res
+}
+
+#' Calculate the Hamming Distance between two kinased graphs
+#'
+#' @param reference a reference result from assign_kinases()
+#' @param comparison a comparison result from assign_kinases
+#'
+#' @return A positive integer
+#' @export
+#'
+#' @examples
+#' TRUE
+kinnet_hamming <- function(reference, comparison) {
+
+  pair <- equalize_kinase_graphs(reference, comparison)
+
+  res <- bnlearn::hamming(pair$reference, pair$comparison)
+
+  res
+}
---
+++ R/equalize_kinase_graphs.R
@@ -0,0 +1,38 @@
+#' Equalize the nodesets of two kinased graphs
+#'
+#' @param reference a reference result from assign_kinases()
+#' @param comparison a comparison result from assign_kinases
+#'
+#' @return A list with two elements, reference and comparison, that have the
+#' same nodesets
+#' @export
+#'
+#' @importFrom bnlearn nodes
+#'
+#' @examples
+#' TRUE
+equalize_kinase_graphs <- function(reference, comparison) {
+  ref_kinased <- render_reduced_kinased_graph(reference, title = "Reference")
+  ref_bnnet <- ref_kinased$bnnet
+  comp_kinased <- render_reduced_kinased_graph(comparison, title = "Comparison")
+  comp_bnnet <- comp_kinased$bnnet
+
+  all_nodes <- unique(c(bnlearn::nodes(ref_bnnet), bnlearn::nodes(comp_bnnet)))
+
+  ref_bnnet_augmented <- ref_bnnet
+  for (node in all_nodes) {
+    ref_bnnet_augmented <- safely_add_node(ref_bnnet_augmented, node)
+  }
+
+  comp_bnnet_augmented <- comp_bnnet
+  for (node in all_nodes) {
+    comp_bnnet_augmented <- safely_add_node(comp_bnnet_augmented, node)
+  }
+
+  out <- list(
+    reference = ref_bnnet_augmented,
+    comparison = comp_bnnet_augmented
+  )
+
+  out
+}
