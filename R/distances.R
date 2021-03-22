31a71bacb8c0f5e2276b12bc42fa01ef5053da8e
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-03-22 14:44:55 -0400

    Added the normalized distance metrics

--- R/distances.R
+++ R/distances.R
@@ -39,3 +39,56 @@ kinnet_hamming <- function(reference, comparison) {
 
   res
 }
+
+
+#' Calculate Normalized Structural Hamming Distance
+#'
+#' @param reference a reference result from assign_kinases()
+#' @param comparison a comparison result from assign_kinases()
+#'
+#' @return A positive integer
+#' @export
+#'
+#' @importFrom bnlearn shd
+#'
+#' @examples
+#' TRUE
+kinnet_normalized_shd <- function(reference, comparison) {
+
+  pair <- equalize_kinase_graphs(reference, comparison)
+
+  shd <- bnlearn::shd(pair$reference, pair$comparison)
+
+  arcnum <- map_dbl(pair, narcs) %>% sum
+
+  norm_shd <-round(shd/arcnum, 5)
+
+  norm_shd
+}
+
+
+#' Calculate Normalized Hamming Distance
+#'
+#' @param reference a reference result from assign_kinases()
+#' @param comparison a reference result from assign_kinases()
+#'
+#' @return A positive integer
+#' @export
+#'
+#' @importFrom bnlearn hamming
+#'
+#' @examples
+#' TRUE
+kinnet_normalized_hamming <- function(reference, comparison) {
+
+  pair <- equalize_kinase_graphs(reference, comparison)
+
+  hamming <- bnlearn::shd(pair$reference, pair$comparison)
+
+  arcnum <- map_dbl(pair, narcs) %>% sum
+
+  norm_hamming <-round(shd/arcnum, 5)
+
+  norm_hamming
+}
+
